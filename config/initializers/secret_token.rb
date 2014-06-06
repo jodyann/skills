cb6f07418c05682ea767254beb2e0beae9585219
Jody Ann <annjodes@gmail.com>
2014-06-06 08:37:19 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NursingSkills::Application.config.secret_key_base = '2d9251b98c9f23505526302e3756166f834052836c352b9da2b7ba966719fa28e16437da1e1cd58ee44ced3bf0231b7f6369654743e9d15c2359a632ce4d66c4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NursingSkills::Application.config.session_store :cookie_store, key: '_nursing_skills_session'
